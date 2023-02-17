b3522bb32ee91f5b85ff6288f7365d09c462b41c
Sandeep Palagati <Sandeep.Palagati2@target.com>
2023-02-17 12:17:03 +0530

    Add test without mocking api

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import App from "./App";
 import { render, screen } from "@testing-library/react";
 
-test("renders learn react link", () => {
+test("renders contains learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
--- src/components/Async.test.jsx
+++ src/components/Async.test.jsx
@@ -2,7 +2,21 @@ import { render, screen } from "@testing-library/react";
 import Async from "./Async";
 
 describe("Async component", () => {
+  //Test 1
+
   test("renders posts if request succeeds", async () => {
+    render(<Async />);
+    const listItemElements = await screen.findAllByRole(
+      "listitem",
+      {},
+      { timeout: 5000 }
+    );
+    expect(listItemElements).not.toHaveLength(0);
+  });
+
+  //Test 2
+
+  test("renders posts if request succeeds through mock", async () => {
     window.fetch = jest.fn();
     window.fetch.mockResolvedValueOnce({
       json: async () => [{ id: 1, titles: "First Post" }],
@@ -15,6 +29,8 @@ describe("Async component", () => {
     );
     expect(listItemElements).not.toHaveLength(0);
   });
+
+  //Test 3
 
   test("component renders if request fails", () => {
     window.fetch = jest.fn();
